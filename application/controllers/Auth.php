0b230c2cc396e07cc207a852849bd430c4b2b6fc
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-08-20 10:41:54 +0700

    commit awal dari web ulitekno
    masih butuh edit untuk penyesuaian lainya

---
+++ application/controllers/Auth.php
@@ -0,0 +1,197 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth extends CI_Controller {
+
+  public function __construct(){
+    parent::__construct();
+    $this->load->library('form_validation');
+  }
+  public function index(){
+    if($this->session->userdata('email')){
+      redirect('user');
+    }
+    $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+    $this->form_validation->set_rules('password', 'Password', 'trim|required');
+    if($this->form_validation->run() == false){
+      $data['title'] = 'KuliTekno - User login';
+      $this->load->view('templates/auth_header', $data);
+      $this->load->view('auth/login');
+      $this->load->view('templates/auth_footer');
+    }else{
+      //validation succeess
+      $this->_login();
+    }
+
+  }
+
+  private function _login(){
+    $email = $this->input->post('email');
+    $password = $this->input->post('password');
+
+    $user = $this->db->get_where('user', ['email' => $email])->row_array();
+
+    //jika user ada
+    if($user){
+      //jika usernya ada
+      if($user['is_active'] == 1){
+        // cek password
+        if(password_verify($password, $user['password'])){
+          $data = [
+            'email' => $user['email'],
+            'role_id' => $user['role_id']
+          ];
+          $this->session->set_userdata($data);
+          if($user['role_id'] == 1){
+            redirect('admin');
+          } else{
+            redirect('user');
+          }
+
+
+        } else {
+          $this->session-> set_flashdata('message', '<div class="alert alert-danger" role="alert"> Wrong password </div>');
+          redirect('auth');
+        }
+      } else{
+        $this->session-> set_flashdata('message', '<div class="alert alert-danger" role="alert"> Email email has not been activated </div>');
+        redirect('auth');
+      }
+    } else {
+      $this->session-> set_flashdata('message', '<div class="alert alert-danger" role="alert"> Email is not registered </div>');
+      redirect('auth');
+    }
+  }
+
+  public function registration(){
+    if($this->session->userdata('email')){
+      redirect('user');
+    }
+    $this->form_validation->set_rules('fullname','Name', 'required|trim');
+    $this->form_validation->set_rules('email','Email', 'required|trim|valid_email|is_unique[user.email]',[
+      'is_unique' => 'this email has already registered'
+    ]);
+    $this->form_validation->set_rules('password1','Password','trim|min_length[3]|matches[password2]', [
+      'matches' => "password don't match",
+      'min_length' => 'password to short'
+    ]);
+    $this->form_validation->set_rules('password2','Password2','trim|matches[password1]');
+
+
+    if($this->form_validation
+    ->run() == false){
+
+      $data['title'] = 'KuliTekno - User Registration';
+      $this->load->view('templates/auth_header', $data);
+      $this->load->view('auth/registration');
+      $this->load->view('templates/auth_footer');
+    } else {
+      $email = $this->input->post('email', true);
+      $data = [
+        'name' => htmlspecialchars($this->input->post('fullname', true)),
+        'email' => htmlspecialchars($email),
+        'image' => 'default.jpg',
+        'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+        'role_id' => 2,
+        'is_active' => 0,
+        'date_create' => time()
+
+      ];
+      $pesan = '<div class="alert alert-success" role="alert"> congratulation '.$this->input->post('fullname').'!! your account has been registered, activation has been sent to your email and will valid until 24 hours/div>';
+      
+      //siapkan token
+      $token = base64_encode(random_bytes(32));
+      $user_token =[
+        'email' => $email,
+        'token' => $token,
+        'date_created' => time()
+      ];
+      $this->db->insert('user',$data);
+      $this->db->insert('user_tpken',$user_token);
+      $this->_sendEmail($token, 'verify');
+
+      $this->session-> set_flashdata('message', $pesan);
+      redirect('auth');
+    }
+  }
+
+  private function _sendEmail($token, $type){
+     $config = [
+       'protocol'  => 'smtp',
+       'smtp_host' => 'ssl://smtp.googlemail.com',
+       'smtp_user' => 'takun917@gmail.com',
+       'smtp_pass' => 'pelatok917',
+       'smtp_port' => 465,
+       'mailtype'  => 'html',
+       'charset' => 'utf-8',
+       'newline' => "\r\n",
+     ];
+     $this->load->library('email', $config);
+     $this->email->from('takun917@gmail.com', 'kuliteno');
+     $this->email->to($this->input->post('email'));
+
+     if($type == 'verify'){
+       $this->email->subject('Account verivication');
+       $this->email->message('click this link to verify our account : <a href="'. base_url() . 'auth/verify?email=' . $this->input->post('email') . '&token=' . urlencode($token) . '">Activate<a>');
+     }
+
+     if($this->email->send()){
+       return true;
+     }else{
+       echo $this->email->print_debugger(); die;
+     }
+
+  }
+
+  public function verify(){
+    $email = $this->input->get('email');
+    $token = $this->input->get('token');
+    $user = $this->db->get_where('user', ['email' => $email])->row_array();
+
+    if($user) {
+      $user_token = $this->db->get_where('user_token', ['token' => $token])->row_array;
+
+      if($user_token) {
+        if(time()-$user_token['date_created'] < (60*60*24)) {
+          $this->db->set('is_active', 1);
+          $this->db->where('email', $email);
+          $this->db->update('user');
+          $this->db->delete('user_token', ['email' => $email]);
+
+          $pesan = '<div class="alert alert-success" role="alert"> ' . $email .' account has been activated!, please login</div>';
+          $this->session-> set_flashdata('message', $pesan);
+          redirect('auth');
+        }else {
+          $this->db->delete('user', ['email' => $email]);
+          $this->db->delete('user_token', ['email' => $email]);
+
+
+          $pesan = '<div class="alert alert-danger" role="alert"> account activation  failed, takon has been expied.</div>';
+          $this->session-> set_flashdata('message', $pesan);
+          redirect('auth');
+        }
+      } else {
+        $pesan = '<div class="alert alert-danger" role="alert"> account activation  failed, wrong token</div>';
+        $this->session-> set_flashdata('message', $pesan);
+        redirect('auth');
+      }
+    } else {
+      $pesan = '<div class="alert alert-danger" role="alert"> account activation dont use to playing man</div>';
+      $this->session-> set_flashdata('message', $pesan);
+      redirect('auth');
+    }
+
+  }
+
+  public function logout(){
+    $this->session->unset_userdata('email');
+    $this->session->unset_userdata('role_id');
+    $pesan = '<div class="alert alert-success" role="alert"> you have been logout</div>';
+    $this->session-> set_flashdata('message', $pesan);
+    redirect('auth');
+
+  }
+  public function blocked(){
+    $this->load->view('auth/blocked');
+  }
+}
---
+++ application/controllers/User.php
@@ -0,0 +1,111 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Controller {
+
+  public function __construct(){
+    parent::__construct();
+    is_logged_in();
+  }
+
+  public function index(){
+    $data['title'] = 'My Profile';
+    $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+      $this->load->view('templates/dash_header', $data);
+      $this->load->view('templates/dash_sidebar', $data);
+      $this->load->view('templates/dash_topbar', $data);
+      $this->load->view('user/index', $data);
+      $this->load->view('templates/dash_footer');
+  }
+  public function edit(){
+    $data['title'] = 'Edit Profile';
+    $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+
+    $this->form_validation->set_rules('name', 'Full Name', 'required|trim');
+    if($this->form_validation->run() == false){
+      $this->load->view('templates/dash_header', $data);
+      $this->load->view('templates/dash_sidebar', $data);
+      $this->load->view('templates/dash_topbar', $data);
+      $this->load->view('user/edit', $data);
+      $this->load->view('templates/dash_footer');
+    }else{
+      $name = $this->input->post('name');
+      $email = $this->input->post('email');
+
+      // cek jika ada gambar terupload
+      $upload_img = $_FILES['image']['name'];
+
+      if($upload_img){
+        $config['allowed_types'] = 'gif|jpg|png|jpeg';
+        $config['max_size']     = '1024';
+        $config['upload_path'] = './assets/img/profile';
+
+        $this->load->library('upload', $config);
+
+        if($this->upload->do_upload('image')){
+          $old_image = $data['user']['image'];
+          if($old_image != 'default.jpg'){
+            unlink(FCPATH . 'assets/img/profile/' . $old_image);
+          }
+          $new_img = $this->upload->data('file_name');
+          $this->db->set('image', $new_img);
+        }else{
+          $pesan = '<div class="alert alert-danger" role="alert">'.$this->upload->display_errors().'</div>';
+          $this->session-> set_flashdata('message', $pesan);
+          redirect('user/edit'); die;
+        }
+      }
+
+      $this->db->set('name',$name);
+      $this->db->where('email', $email);
+      $this->db->update('user');
+
+      $pesan = '<div class="alert alert-success" role="alert"> Your profile has been updated</div>';
+      $this->session-> set_flashdata('message', $pesan);
+      redirect('user');
+    }
+  }
+
+  public function changePassword(){
+    $data['title'] = 'Change Password';
+    $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+    $this->form_validation->set_rules('current_password', 'Current Password', 'required|trim');
+    $this->form_validation->set_rules('new_password1', 'New Password', 'required|trim|min_length[3]|matches[new_password2]');
+    $this->form_validation->set_rules('new_password2', 'Confirm New Password
+    ', 'required|trim|min_length[3]|matches[new_password1]');
+
+    if($this->form_validation->run() == false){
+      $this->load->view('templates/dash_header', $data);
+      $this->load->view('templates/dash_sidebar', $data);
+      $this->load->view('templates/dash_topbar', $data);
+      $this->load->view('user/changepassword', $data);
+      $this->load->view('templates/dash_footer');
+    }else{
+      $current_password = $this->input->post('current_password');
+      $new_password = $this->input->post('new_password1');
+      if(!password_verify($current_password, $data['user']['password'])){
+        $pesan = '<div class="alert alert-danger" role="alert"> wrong current password.</div>';
+        $this->session-> set_flashdata('message', $pesan);
+        redirect('user/changepassword');
+      }else{
+        if($current_password == $new_password){
+          $pesan = '<div class="alert alert-danger" role="alert"> stupid!, your password same with the current.</div>';
+          $this->session-> set_flashdata('message', $pesan);
+          redirect('user/changepassword');
+        } else{
+          //password baru
+          $password_hash = password_hash($new_password, PASSWORD_DEFAULT);
+          $this->db->set('password', $password_hash);
+          $this->db->where('email', $this->session->userdata('email'));
+          $this->db->update('user');
+
+          $pesan = '<div class="alert alert-success" role="alert"> Your password succesful changed.</div>';
+          $this->session-> set_flashdata('message', $pesan);
+          redirect('user/changepassword');
+        }
+      }
+    }
+  }
+}
