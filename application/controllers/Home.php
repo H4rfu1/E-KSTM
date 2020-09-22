b526ec2bbbfd18df3cde17f87847b79ca4303a4e
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-09-22 19:22:27 +0700

    frgot del sidebar

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -8,7 +8,6 @@ class Home extends CI_Controller {
     	$data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
 		}
 		$this->load->view('templates/home_header', $data);
-    $this->load->view('templates/dash_sidebar', $data);
     $this->load->view('templates/home_topbar', $data);
     $this->load->view('home/index', $data);
     $this->load->view('templates/home_footer');
