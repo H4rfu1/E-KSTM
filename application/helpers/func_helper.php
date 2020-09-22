a1b851cadd13b5c05c9d91e8f41ea3173151cc87
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-09-22 18:41:39 +0700

    Update func_helper.php

--- application/helpers/func_helper.php
+++ application/helpers/func_helper.php
@@ -19,10 +19,9 @@
         $userAccess = $CI->db->get_where('user_access_menu', ['role_id' => $role_id, 'menu_id' => $menu_id]);
         if($userAccess->num_rows() < 1){
           redirect('auth/blocked');
+      }
       }elseif ($da == "home") {
         return true;
-      }
-
       }
     }
 
