9016c9aba934296cefde41413c98b1c231020961
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-09-22 18:08:58 +0700

    add logo top bar
    replace search bar with logo

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -34,22 +34,14 @@
         <!-- Topbar -->
         <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-          <!-- Sidebar Toggle (Topbar) -->
-          <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-            <i class="fa fa-bars"></i>
-          </button>
 
-          <!-- Topbar Search -->
-          <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-            <div class="input-group">
-              <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-              <div class="input-group-append">
-                <button class="btn btn-primary" type="button">
-                  <i class="fas fa-search fa-sm"></i>
-                </button>
-              </div>
+          <!-- Logo -->
+          <a class="top-brand d-flex align-items-center justify-content-center" href="index.html">
+            <div class="top-brand-icon rotate-n-15">
+              <i class="fas fa-laugh-wink"></i>
             </div>
-          </form>
+            <div class="top-brand-text mx-3">SB Admin <sup>2</sup></div>
+          </a>
 
           <!-- Topbar Navbar -->
           <ul class="navbar-nav ml-auto">
