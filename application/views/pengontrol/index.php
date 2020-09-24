cf2c8d2f06e1fa5f2b3a27886696061a770942d4
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-09-24 23:53:05 +0700

    pengontrol - fix del url

--- application/views/pengontrol/index.php
+++ application/views/pengontrol/index.php
@@ -62,8 +62,8 @@
             <td><?= $r['foto'] ?></td>
             <td><?= $r['video'] ?></td>
           <td>
-            <a href="<?= base_url('kstm/edit/'); echo $r['id_laporan_pengontrol']; ?>" class="badge badge-primary">Edit</a>
-            <a href="<?= base_url('kstm/delete_laporan_pengontrol/'); echo $r['id_laporan_pengontrol']; ?>" class="badge badge-danger" onclick="return confirm('yakin'); ">Delete</a>
+            <a href="<?= base_url('pengonrol/edit/'); echo $r['id_laporan_pengontrol']; ?>" class="badge badge-primary">Edit</a>
+            <a href="<?= base_url('pengonrol/delete_laporan_pengontrol/'); echo $r['id_laporan_pengontrol']; ?>" class="badge badge-danger" onclick="return confirm('yakin'); ">Delete</a>
           </td>
         </tr>
       <?php $i++; endforeach; ?>
