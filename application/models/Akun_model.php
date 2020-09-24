996309f6b647d0adf26a49e88d75f61438743f2b
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-09-24 19:09:58 +0700

    Update Akun_model.php

--- application/models/Akun_model.php
+++ application/models/Akun_model.php
@@ -6,7 +6,7 @@ class Akun_model extends CI_Model {
      $query = "SELECT `user`.*, `user_role`.`role`
               FROM `user` JOIN `user_role`
               ON `user`.`role_id` = `user_role`.`id`
-              WHERE (`user`.`role_id` != 1) OR (`user`.`role_id` != 4)";
+              WHERE `user`.`role_id` != 1";
       return $this->db->query($query)->result_array();
   }
 }
