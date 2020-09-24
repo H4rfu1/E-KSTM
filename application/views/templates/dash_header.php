92fed29cdf02a8b5f56fc739522b82e7d82ec926
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-09-25 06:53:57 +0700

    a hover pointer

--- application/views/templates/dash_header.php
+++ application/views/templates/dash_header.php
@@ -26,6 +26,12 @@
   <!-- Custom styles for this template-->
   <link href="<?= base_url('assets/'); ?>css/sb-admin-2.min.css" rel="stylesheet">
 
+	<style type="text/css">
+ a:hover {
+  cursor:pointer;
+ }
+</style>
+
 </head>
 
 <body id="page-top">
