41aa63be3a729c463206f47815fab684f3477396
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-09-25 06:33:43 +0700

    css button confirm

--- application/views/templates/dash_footer.php
+++ application/views/templates/dash_footer.php
@@ -53,7 +53,7 @@
 <script type="text/javascript">
 function conDelete(url) {
   bootbox.confirm("Confirm Delete?", function(result){
-    console.log(url);
+    window.location.href = url;
   });
 }
 
