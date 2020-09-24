845d3421403a0ecb9376d71f1e348f6fe3262188
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-09-24 23:43:23 +0700

    pengontrol - fix action url

--- application/views/pengontrol/index.php
+++ application/views/pengontrol/index.php
@@ -91,7 +91,7 @@
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <form class="" action="<?= base_url('kstm') ?>" method="post">
+      <form class="" action="<?= base_url('pengontrol') ?>" method="post">
       <div class="modal-body">
         <div class="input-group form-group">
           <div class="input-group-prepend">
