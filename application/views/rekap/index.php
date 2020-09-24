1b242662d19f41d68342b0d2a3c328afca656f44
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-09-25 05:37:32 +0700

    mb 4 dropdown

--- application/views/rekap/index.php
+++ application/views/rekap/index.php
@@ -6,8 +6,8 @@
 
 <div class="row">
   <div class="col-lg-12 table-responsive">
-    <div class="btn-group dropright mb-1">
-      <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <div class="btn-group dropright mb-4">
+      <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         Laporan
       </button>
       <div class="dropdown-menu" >
