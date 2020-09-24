e26fcba73399ef6f12c3a7266078ce2b7032b380
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-09-25 05:38:04 +0700

    Update index.php

--- application/views/rekap/index.php
+++ application/views/rekap/index.php
@@ -6,7 +6,7 @@
 
 <div class="row">
   <div class="col-lg-12 table-responsive">
-    <div class="btn-group dropright mb-4">
+    <div class="btn-group dropright mb-5">
       <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         Laporan
       </button>
