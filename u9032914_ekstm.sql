f273aadfefd0700282447610b7e099a234d627c1
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-09-24 23:56:56 +0700

    Update u9032914_ekstm.sql

--- u9032914_ekstm.sql
+++ u9032914_ekstm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Sep 24, 2020 at 11:40 AM
+-- Generation Time: Sep 24, 2020 at 11:56 PM
 -- Server version: 10.3.23-MariaDB
 -- PHP Version: 7.3.6
 
@@ -41,19 +41,6 @@ CREATE TABLE `forum` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `instansi`
---
-
-CREATE TABLE `instansi` (
-  `id_instansi` int(11) NOT NULL,
-  `nama_instansi` int(11) NOT NULL,
-  `alamat` int(11) DEFAULT NULL,
-  `keterangan` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `laporan_kstm`
 --
 
@@ -76,6 +63,14 @@ CREATE TABLE `laporan_kstm` (
   `harga_ternak_perekor` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `laporan_kstm`
+--
+
+INSERT INTO `laporan_kstm` (`id_laporan_kstm`, `id_pelapor`, `tanggal_laporan`, `deskripsi_laporan`, `jenis_ternak`, `jumlah_ternak_sebelumnya`, `jumlah_ternak_sekarang`, `jumlah_ternak_meninggal`, `keterangan_ternak_meninggal`, `jumlah_ternak_sehat`, `jumlah_ternak_sakit`, `keterangan_kesehatan_ternak`, `jumlah_ternak_dikonsumsi`, `keterangan_konsumsi`, `jumlah_ternak_dijual`, `harga_ternak_perekor`) VALUES
+(2, 40, 1600963722, '1', '1', 11, 1, 1, '1', 1, 1, '1', 1, '1', 1, 1),
+(3, 43, 1600965723, 'a', '1', 1, 1, 1, '1', 1, 1, '1', 1, '1', 1, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -91,6 +86,7 @@ CREATE TABLE `laporan_pengontrol` (
   `jumlah_ternak_sebelumnya` int(11) NOT NULL,
   `jumlah_ternak_sekarang` int(11) NOT NULL,
   `jumlah_ternak_meninggal` int(11) NOT NULL,
+  `keterangan_ternak_meninggal` varchar(256) NOT NULL,
   `jumlah_ternak_sehat` int(11) NOT NULL,
   `jumlah_ternak_sakit` int(11) NOT NULL,
   `keterangan_kesehatan_ternak` varchar(256) NOT NULL,
@@ -98,8 +94,8 @@ CREATE TABLE `laporan_pengontrol` (
   `keterangan_konsumsi` varchar(256) NOT NULL,
   `jumlah_ternak_dijual` int(11) NOT NULL,
   `harga_ternak_perekor` int(11) NOT NULL,
-  `foto` varchar(128) NOT NULL,
-  `video` varchar(128) NOT NULL
+  `foto` varchar(128) DEFAULT NULL,
+  `video` varchar(128) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -111,11 +107,19 @@ CREATE TABLE `laporan_pengontrol` (
 CREATE TABLE `pemberitahuan` (
   `id_pemberitahuan` int(11) NOT NULL,
   `id_pengirim` int(11) NOT NULL,
-  `id_penerima` int(11) NOT NULL,
+  `id_role` int(11) NOT NULL,
   `tanggal_pemberitahuan` int(11) NOT NULL,
-  `isi_pemberitahuan` varchar(256) NOT NULL,
-  `foto` varchar(128) NOT NULL
+  `isi_pemberitahuan` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pemberitahuan`
+--
+
+INSERT INTO `pemberitahuan` (`id_pemberitahuan`, `id_pengirim`, `id_role`, `tanggal_pemberitahuan`, `isi_pemberitahuan`) VALUES
+(2, 39, 7, 1600955937, 'asdfdaf'),
+(3, 39, 2, 1600956366, 'selamat datang'),
+(4, 39, 4, 1600956386, 'buatkan email ya');
 
 -- --------------------------------------------------------
 
@@ -145,19 +149,23 @@ CREATE TABLE `user` (
   `password` varchar(256) NOT NULL,
   `role_id` int(11) NOT NULL,
   `is_active` int(1) NOT NULL,
-  `date_create` int(11) NOT NULL,
-  `instansi_id` int(11) DEFAULT NULL
+  `date_create` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `name`, `email`, `image`, `password`, `role_id`, `is_active`, `date_create`, `instansi_id`) VALUES
-(25, 'Moh. Fahrul Hafidh', 'sakun915@gmail.com', 'default.jpg', '$2y$10$TOr1i6vLxzysM53Uv6Ofx.wXpGpTijaQOjDsaZPo3h2ppCWFrqwC2', 4, 1, 1600312024, NULL),
-(27, 's', 'dakun916@gmail.com', 'default.jpg', '$2y$10$xulyhbru7pes6TlDnzmHae61cjR7sPcbgm4ntjvAp4dQbLXKmN3Ku', 1, 1, 1600312890, NULL),
-(31, 'Alif Aditya Rahman', 'alifadityarahman18@gmail.com', 'default.jpg', '$2y$10$nf1zV.o/GDA6yvqqxeL3ge2nqWLnxReBP4SU2n3XO10UWC0WZvWgm', 2, 1, 1600317800, NULL),
-(33, 'Adit Aladra', 'adit.aladra@gmail.com', 'default.jpg', '$2y$10$eqy9lwL84jBZ6v8ED2TeX.5mXrpZ1dkx1XrAU2HJAoPgnPkZPLCom', 2, 1, 1600318344, NULL);
+INSERT INTO `user` (`id`, `name`, `email`, `image`, `password`, `role_id`, `is_active`, `date_create`) VALUES
+(25, 'Moh. Fahrul Hafidh', 'sakun915@gmail.com', 'default.jpg', '$2y$10$TOr1i6vLxzysM53Uv6Ofx.wXpGpTijaQOjDsaZPo3h2ppCWFrqwC2', 4, 1, 1600312024),
+(27, 's', 'dakun916@gmail.com', 'default.jpg', '$2y$10$xulyhbru7pes6TlDnzmHae61cjR7sPcbgm4ntjvAp4dQbLXKmN3Ku', 1, 1, 1600312890),
+(31, 'Alif Aditya Rahman', 'alifadityarahman18@gmail.com', 'default.jpg', '$2y$10$nf1zV.o/GDA6yvqqxeL3ge2nqWLnxReBP4SU2n3XO10UWC0WZvWgm', 2, 1, 1600317800),
+(33, 'Adit Aladra', 'adit.aladra@gmail.com', 'default.jpg', '$2y$10$eqy9lwL84jBZ6v8ED2TeX.5mXrpZ1dkx1XrAU2HJAoPgnPkZPLCom', 2, 1, 1600318344),
+(39, 'tst', 'test@test.id', 'default.jpg', '$2y$10$RMR8vxGKOCXmMOt0PxiHG.WjLLqRyNFHVPV/qI/5y4nowmMqEZjy2', 3, 1, 1600950084),
+(40, 'kstm', 'kstm@gmail.com', 'default.jpg', '$2y$10$u93.h2mfgUIdUgnZqQJdpepkVZBK81S.Jq6xI7wAB9a35VmqptqUW', 7, 1, 1600957480),
+(41, 'A', 'a@gmail.com', 'default.jpg', '$2y$10$lAtHzgKPZcRopnmnJDFUFezijN0HJRmQZcG9heUGCY0NGkGFpb3LG', 2, 0, 1600958496),
+(42, 'Nsnsjsks', 'yaqinsuainul1@gmail.com', 'default.jpg', '$2y$10$67n7jZpU91IeEtsE1PtMHu9d.4ss2BUlsO25ux1L.TqkA4qd2l1pu', 2, 1, 1600960366),
+(43, 'pelapor', 'pengontrol@pelapor.com', 'default.jpg', '$2y$10$0JpROAJc9bFn.59tCbsPquOLb10AgBDmtrnNnqKlFzTSmJPGfSt/y', 8, 1, 1600964040);
 
 -- --------------------------------------------------------
 
@@ -283,8 +291,8 @@ INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active
 (6, 3, 'Submenu Management', 'menu/submenu', 'fas fa-fw fa-folder-open', 1),
 (9, 1, 'Role', 'admin/role', 'fas fa-fw fa-user-tie', 1),
 (10, 2, 'Change Password', 'user/changepassword', 'fas fa-fw fa-key', 1),
-(14, 6, 'Kelola Laporan', 'kstm', 'fas fa-fw fa-cogs', 1),
-(15, 7, 'Kelola Laporan', 'pengontrol', 'fas fa-fw fa-cogs', 1),
+(14, 6, 'Kelola Laporan', 'kstm', 'fas fa-fw fa-file-alt', 1),
+(15, 7, 'Kelola Laporan', 'pengontrol', 'fas fa-fw fa-file-invoice', 1),
 (16, 8, 'Rekap', 'rekap', 'fas fa-fw fa-folder-open', 1),
 (17, 9, 'Pengumuman', 'pengumuman', 'fas fa-fw fa-bullhorn', 1),
 (18, 10, 'Forum', 'forum', 'fas fa-fw fa-comments', 1),
@@ -304,6 +312,13 @@ CREATE TABLE `user_token` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `user_token`
+--
+
+INSERT INTO `user_token` (`id`, `email`, `token`, `date_created`) VALUES
+(25, 'a@gmail.com', 'MRw64J7R4C61T+tkMqtTOCz5jrVsEo+P1xdwvoJRAKg=', 1600958496);
+
+--
 -- Indexes for dumped tables
 --
 
@@ -313,12 +328,6 @@ CREATE TABLE `user_token` (
 ALTER TABLE `forum`
   ADD PRIMARY KEY (`id_forum`),
   ADD KEY `id_pembuat` (`id_pembuat`);
-
---
--- Indexes for table `instansi`
---
-ALTER TABLE `instansi`
-  ADD PRIMARY KEY (`id_instansi`);
 
 --
 -- Indexes for table `laporan_kstm`
@@ -340,7 +349,7 @@ ALTER TABLE `laporan_pengontrol`
 ALTER TABLE `pemberitahuan`
   ADD PRIMARY KEY (`id_pemberitahuan`),
   ADD KEY `id_pengirim` (`id_pengirim`),
-  ADD KEY `id_penerima` (`id_penerima`);
+  ADD KEY `id_role` (`id_role`);
 
 --
 -- Indexes for table `tanggapan_forum`
@@ -355,8 +364,7 @@ ALTER TABLE `tanggapan_forum`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `role_id` (`role_id`),
-  ADD KEY `instansi_id` (`instansi_id`);
+  ADD KEY `role_id` (`role_id`);
 
 --
 -- Indexes for table `user_access_menu`
@@ -399,28 +407,22 @@ ALTER TABLE `forum`
   MODIFY `id_forum` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `instansi`
---
-ALTER TABLE `instansi`
-  MODIFY `id_instansi` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `laporan_kstm`
 --
 ALTER TABLE `laporan_kstm`
-  MODIFY `id_laporan_kstm` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_laporan_kstm` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `laporan_pengontrol`
 --
 ALTER TABLE `laporan_pengontrol`
-  MODIFY `id_laporan_pengontrol` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_laporan_pengontrol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `pemberitahuan`
 --
 ALTER TABLE `pemberitahuan`
-  MODIFY `id_pemberitahuan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pemberitahuan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tanggapan_forum`
@@ -432,7 +434,7 @@ ALTER TABLE `tanggapan_forum`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `user_access_menu`
@@ -462,7 +464,7 @@ ALTER TABLE `user_sub_menu`
 -- AUTO_INCREMENT for table `user_token`
 --
 ALTER TABLE `user_token`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- Constraints for dumped tables
@@ -491,7 +493,7 @@ ALTER TABLE `laporan_pengontrol`
 --
 ALTER TABLE `pemberitahuan`
   ADD CONSTRAINT `pemberitahuan_ibfk_1` FOREIGN KEY (`id_pengirim`) REFERENCES `user` (`id`),
-  ADD CONSTRAINT `pemberitahuan_ibfk_2` FOREIGN KEY (`id_penerima`) REFERENCES `user` (`id`);
+  ADD CONSTRAINT `pemberitahuan_ibfk_3` FOREIGN KEY (`id_role`) REFERENCES `user_role` (`id`);
 
 --
 -- Constraints for table `tanggapan_forum`
@@ -504,8 +506,7 @@ ALTER TABLE `tanggapan_forum`
 -- Constraints for table `user`
 --
 ALTER TABLE `user`
-  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `user_role` (`id`),
-  ADD CONSTRAINT `user_ibfk_2` FOREIGN KEY (`instansi_id`) REFERENCES `instansi` (`id_instansi`);
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `user_role` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
