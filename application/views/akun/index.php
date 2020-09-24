33cc7ca01b5b89b758692f774a9639d074dda578
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-09-25 06:31:09 +0700

    chang type to button

--- application/views/akun/index.php
+++ application/views/akun/index.php
@@ -41,7 +41,7 @@
             <td><?= date('d F Y', $user['date_create']); ?></td>
           <td>
             <a href="<?= base_url('akun/edit/'); echo $r['id']; ?>" class="badge badge-primary">Edit</a>
-            <a href="" class="badge badge-danger" onclick="conDelete('<?= base_url("akun/delete_akun/"); echo $r['id']; ?>'); ">Delete</a>
+            <button class="badge badge-danger" onclick="conDelete('<?= base_url("akun/delete_akun/"); echo $r['id']; ?>'); ">Delete</button>
           </td>
         </tr>
       <?php $i++; endforeach; ?>
