21137ad3f5673d22900ecd887ed1c50dc70dc07f
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-09-25 18:57:46 +0700

    Update edit.php

--- application/views/akun/edit.php
+++ application/views/akun/edit.php
@@ -6,7 +6,7 @@
 
 <div class="row">
   <div class="col-lg-8">
-      <form class="" action="<?= base_url('akun/edit/'); echo $user['id'] ?>" method="post">
+      <form class="" action="<?= base_url('akun/edit/'); echo $user['id']; ?>" method="post">
       <div class="form-group row">
         <label for="email" class="col-sm-2 col-form-label" >Email</label>
         <div class="col-sm-10">
