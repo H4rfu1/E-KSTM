83c85241b8357d2055b59c7b2b1e75fc4cd46876
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-09-26 04:49:10 +0700

    forum

--- application/views/templates/dash_header.php
+++ application/views/templates/dash_header.php
@@ -28,6 +28,7 @@
 
   <!-- Custom styles for this template-->
   <link href="<?= base_url('assets/'); ?>css/sb-admin-2.min.css" rel="stylesheet">
+	<link href="<?= base_url('assets/'); ?>css/blog-home.css" rel="stylesheet">
 
 	<style type="text/css">
  a:hover {
