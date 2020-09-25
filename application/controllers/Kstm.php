2febd38e9e862390245ef3c9d1ab316cfa9f8b51
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-09-25 18:14:36 +0700

    edit  - laporan pengontrol

--- application/controllers/Kstm.php
+++ application/controllers/Kstm.php
@@ -90,37 +90,6 @@ class Kstm extends CI_Controller {
       $this->load->view('kstm/edit', $data);
       $this->load->view('templates/dash_footer');
     }else{
-      // $deskripsi_laporan = htmlspecialchars( $this->input->post('deskripsi_laporan'));
-      // $jenis_ternak = htmlspecialchars( $this->input->post('jenis_ternak'));
-      // $jumlah_ternak_sebelumnya = htmlspecialchars( $this->input->post('jumlah_ternak_sebelumnya'));
-      // $jumlah_ternak_sekarang = htmlspecialchars( $this->input->post('jumlah_ternak_sekarang'));
-      // $jumlah_ternak_meninggal = htmlspecialchars( $this->input->post('jumlah_ternak_meninggal'));
-      // $keterangan_ternak_meninggal = htmlspecialchars( $this->input->post('keterangan_ternak_meninggal'));
-      // $jumlah_ternak_sehat = htmlspecialchars( $this->input->post('jumlah_ternak_sehat'));
-      // $jumlah_ternak_sakit = htmlspecialchars( $this->input->post('jumlah_ternak_sakit'));
-      // $keterangan_kesehatan_ternak = htmlspecialchars( $this->input->post('keterangan_kesehatan_ternak'));
-      // $jumlah_ternak_dikonsumsi = htmlspecialchars( $this->input->post('jumlah_ternak_dikonsumsi'));
-      // $keterangan_konsumsi = htmlspecialchars( $this->input->post('keterangan_konsumsi'));
-      // $jumlah_ternak_dijual = htmlspecialchars( $this->input->post('jumlah_ternak_dijual'));
-      // $harga_ternak_perekor = htmlspecialchars( $this->input->post('harga_ternak_perekor'));
-      //
-      //
-      // $this->db->set('deskripsi_laporan',$deskripsi_laporan);
-      // $this->db->set('jenis_ternak',$jenis_ternak);
-      // $this->db->set('jumlah_ternak_sebelumnya',$jumlah_ternak_sebelumnya);
-      // $this->db->set('jumlah_ternak_sekarang',$jumlah_ternak_sekarang);
-      // $this->db->set('jumlah_ternak_meninggal',$jumlah_ternak_meninggal);
-      // $this->db->set('keterangan_ternak_meninggal',$keterangan_ternak_meninggal);
-      // $this->db->set('jumlah_ternak_sehat',$jumlah_ternak_sehat);
-      // $this->db->set('jumlah_ternak_sakit',$jumlah_ternak_sakit);
-      // $this->db->set('keterangan_kesehatan_ternak',$keterangan_kesehatan_ternak);
-      // $this->db->set('jumlah_ternak_dikonsumsi',$jumlah_ternak_dikonsumsi);
-      // $this->db->set('keterangan_konsumsi',$keterangan_konsumsi);
-      // $this->db->set('jumlah_ternak_dijual',$jumlah_ternak_dijual);
-      // $this->db->set('harga_ternak_perekor',$harga_ternak_perekor);
-      // $this->db->where('id_laporan_kstm', $id);
-      // $this->db->update('laporan_kstm');
-
       $data = [
         'deskripsi_laporan' => htmlspecialchars( $this->input->post('deskripsi_laporan')),
         'jenis_ternak' => htmlspecialchars( $this->input->post('jenis_ternak')),
@@ -138,8 +107,10 @@ class Kstm extends CI_Controller {
 
       ];
       $this->db->update('laporan_kstm', $data, array('id_laporan_kstm' => $id));
-      $pesan = '<div class="alert alert-success" role="alert"> Laporan has been updated </div>';
-      $this->session-> set_flashdata('message', $pesan);
+      if ($this->db->affected_rows() > 0) {
+        $pesan = '<div class="alert alert-success" role="alert"> Laporan has been updated </div>';
+        $this->session-> set_flashdata('message', $pesan);
+      }
       redirect('kstm');
     }
   }
--- application/controllers/Pengontrol.php
+++ application/controllers/Pengontrol.php
@@ -15,7 +15,7 @@ class Pengontrol extends CI_Controller {
     $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
 
     $id_user = $this->session->userdata('id');
-    $this->db->where("id_pelapor != $id_user");
+    $this->db->where("id_pelapor = $id_user");
     $data['laporan_pengontrol'] = $this->db->get('laporan_pengontrol')->result_array();
 
 
@@ -91,8 +91,8 @@ class Pengontrol extends CI_Controller {
         'keterangan_konsumsi' => htmlspecialchars( $this->input->post('keterangan_konsumsi')),
         'jumlah_ternak_dijual' => htmlspecialchars( $this->input->post('jumlah_ternak_dijual')),
         'harga_ternak_perekor' => htmlspecialchars( $this->input->post('harga_ternak_perekor')),
-        'tanggal_laporan' => $new_img,
-        'tanggal_laporan' =>$new_vid
+        'foto' => $new_img,
+        'video' =>$new_vid
       ];
       $this->db->insert('laporan_pengontrol', $data);
       $this->session-> set_flashdata('message', '<div class="alert alert-success" role="alert"> Laporan added </div>');
@@ -101,8 +101,90 @@ class Pengontrol extends CI_Controller {
 
   }
 
-  public function edit($id){
-    echo "edit id $id";
+  public function edit($id = 0){
+    $data['title'] = 'Edit Profile';
+    $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+    $data['laporan'] = $this->db->get_where('laporan_pengontrol', ['id_laporan_pengontrol' => $id])->row_array();
+
+    $this->form_validation->set_rules('deskripsi_laporan','Deskripsi_laporan', 'required');
+    $this->form_validation->set_rules('jenis_ternak','Jenis_ternak', 'required');
+    $this->form_validation->set_rules('jumlah_ternak_sebelumnya','Jumlah_ternak_sebelumnya', 'required');
+    $this->form_validation->set_rules('jumlah_ternak_sekarang','Jumlah_ternak_sekarang', 'required');
+    $this->form_validation->set_rules('jumlah_ternak_meninggal','Jumlah_ternak_meninggal', 'required');
+    $this->form_validation->set_rules('keterangan_ternak_meninggal','Keterangan_ternak_meninggal', 'required');
+    $this->form_validation->set_rules('jumlah_ternak_sehat','Jumlah_ternak_sehat', 'required');
+    $this->form_validation->set_rules('jumlah_ternak_sakit','Jumlah_ternak_sakit', 'required');
+    $this->form_validation->set_rules('keterangan_kesehatan_ternak','Keterangan_kesehatan_ternak', 'required');
+    $this->form_validation->set_rules('jumlah_ternak_dikonsumsi','Jumlah_ternak_dikonsumsi', 'required');
+    $this->form_validation->set_rules('keterangan_konsumsi','Keterangan_konsumsi', 'required');
+    $this->form_validation->set_rules('jumlah_ternak_dijual','Jumlah_ternak_dijual', 'required');
+    $this->form_validation->set_rules('harga_ternak_perekor','Harga_ternak_perekor', 'required');
+    if($this->form_validation->run() == false and $id != 0){
+      $this->load->view('templates/dash_header', $data);
+      $this->load->view('templates/dash_sidebar', $data);
+      $this->load->view('templates/dash_topbar', $data);
+      $this->load->view('pengontrol/edit', $data);
+      $this->load->view('templates/dash_footer');
+    }else{
+      $new_img = '';
+      $new_vid = '';
+      // cek jika ada gambar terupload
+      if ($_FILES['image']['size'] == 0) {
+        $upload_img = $_FILES['image']['name'];
+        if($upload_img){
+          $config['allowed_types'] = 'gif|jpg|png|jpeg';
+          $config['max_size']     = '1024';
+          $config['upload_path'] = './assets/img/profile';
+
+          $this->load->library('upload', $config);
+
+          if($this->upload->do_upload('image')){
+            $new_img = $this->upload->data('file_name');
+          }
+        }
+      }
+
+      // cek jika ada video terupload
+      if ($_FILES['video']['size'] == 0) {
+        $upload_vid = $_FILES['video']['name'];
+        if($upload_vid){
+          $config['allowed_types'] = 'mp4|3gp|avi|flv|webm|wmv';
+          $config['max_size']     = '5120';
+          $config['upload_path'] = './assets/vid/pengontrol';
+
+          $this->load->library('upload', $config);
+
+          if($this->upload->do_upload('video')){
+            $new_vid = $this->upload->data('file_name');
+          }
+        }
+      }
+
+      $data = [
+        'deskripsi_laporan' => htmlspecialchars( $this->input->post('deskripsi_laporan')),
+        'jenis_ternak' => htmlspecialchars( $this->input->post('jenis_ternak')),
+        'jumlah_ternak_sebelumnya' => htmlspecialchars( $this->input->post('jumlah_ternak_sebelumnya')),
+        'jumlah_ternak_sekarang' => htmlspecialchars( $this->input->post('jumlah_ternak_sekarang')),
+        'jumlah_ternak_meninggal' => htmlspecialchars( $this->input->post('jumlah_ternak_meninggal')),
+        'keterangan_ternak_meninggal' => htmlspecialchars( $this->input->post('keterangan_ternak_meninggal')),
+        'jumlah_ternak_sehat' => htmlspecialchars( $this->input->post('jumlah_ternak_sehat')),
+        'jumlah_ternak_sakit' => htmlspecialchars( $this->input->post('jumlah_ternak_sakit')),
+        'keterangan_kesehatan_ternak' => htmlspecialchars( $this->input->post('keterangan_kesehatan_ternak')),
+        'jumlah_ternak_dikonsumsi' => htmlspecialchars( $this->input->post('jumlah_ternak_dikonsumsi')),
+        'keterangan_konsumsi' => htmlspecialchars( $this->input->post('keterangan_konsumsi')),
+        'jumlah_ternak_dijual' => htmlspecialchars( $this->input->post('jumlah_ternak_dijual')),
+        'harga_ternak_perekor' => htmlspecialchars( $this->input->post('harga_ternak_perekor')),
+        'foto' => $new_img,
+        'video' =>$new_vid
+
+      ];
+      $this->db->update('laporan_pengontrol', $data, array('id_laporan_pengontrol' => $id));
+      if ($this->db->affected_rows() > 0) {
+        $pesan = '<div class="alert alert-success" role="alert"> Laporan has been updated </div>';
+        $this->session-> set_flashdata('message', $pesan);
+      }
+      redirect('pengontrol');
+    }
   }
 
   public function delete_laporan_pengontrol($id){
