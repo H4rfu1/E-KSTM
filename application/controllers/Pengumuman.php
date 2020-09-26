d5680a1628a110f69447225f6226eb69e6acb4b8
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-09-26 18:50:08 +0700

    guest pemberitahuan

--- application/controllers/Pengumuman.php
+++ application/controllers/Pengumuman.php
@@ -23,6 +23,7 @@ class Pengumuman extends CI_Controller {
     }
 
     $this->db->where('id != 1');
+    $this->db->where('id != 2');
     $this->db->where('id != 3');
     $data['role'] = $this->db->get('user_role')->result_array();
 
