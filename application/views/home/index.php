b57925c506e07f80660461ceade064dbbcfc8903
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-09-27 06:41:29 +0700

    nav home edit

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -21,17 +21,49 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
             <div class="container">
-                <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
-                <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i class="fas fa-bars"></i>
-                </button>
+                <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="<?= base_url('assets/img/');?>logo.png" class="d-inline-block align-middle mr-2" width="60" alt="logo"></a>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
+                  <!-- Topbar Navbar -->
+                  <?php if (is_logged_in("home")) {
+                    echo '
+                      <ul class="navbar-nav ml-auto">
+                        <li class="nav-item dropdown no-arrow mx-1">
+                            <a class="btn btn-primary" href="'. base_url("auth/registration") .'" role="button">Daftar</a>
+                        </li>
+                        <li class="nav-item dropdown no-arrow mx-1">
+                            <a class="btn btn-primary" href="'. base_url("auth") .'" role="button">Masuk</a>
+                        </li>
+                      </ul>
+                    ';
+                  } else {
+                    echo '
                     <ul class="navbar-nav ml-auto">
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Portfolio</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">About</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Contact</a></li>
+
+                      <div class="topbar-divider d-none d-sm-block"></div>
+
+                      <!-- Nav Item - User Information -->
+                      <li class="nav-item dropdown no-arrow">
+                        <a class="nav-link dropdown-toggle" href="'. base_url('user').'" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          <span class="mr-2 d-none d-lg-inline text-gray-600 small">'. $user['name'] .'</span>
+                          <img class="img-profile rounded-circle" src="'. base_url('assets/img/profile/'). $user['image'] .'">
+                        </a>
+                        <!-- Dropdown - User Information -->
+                        <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                          <a class="dropdown-item" href="'. base_url('user').'">
+                            <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Profil
+                          </a>
+                          <div class="dropdown-divider"></div>
+                          <a class="dropdown-item" href="'.base_url('auth/logout') .'" data-toggle="modal" data-target="#logoutModal">
+                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Keluar
+                          </a>
+                        </div>
+                      </li>
+
                     </ul>
+                  ';
+                  }?>
                 </div>
             </div>
         </nav>
