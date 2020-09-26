bb8bdcf317d1323b22fd20c4ef756a9fcd99b98d
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-09-26 17:53:48 +0700

    center image

--- application/views/forum/diskusi.php
+++ application/views/forum/diskusi.php
@@ -58,7 +58,7 @@
          foreach($komen as $k) :
         ?>
         <div class="media mb-4">
-          <img class="d-flex mr-3 rounded-circle" src="<?= base_url('assets/img/profile/') . $k['image']; ?>" alt="foto" width="50" height="50">
+          <img class="mx-auto d-block rounded-circle" src="<?= base_url('assets/img/profile/') . $k['image']; ?>" alt="foto" width="50" height="50">
           <div class="media-body">
             <h5 style="color: black;" class="mt-0"><?= $k['name'] ?></h5>
             <p><?= $k['isi_tanggapan'] ?></p>
