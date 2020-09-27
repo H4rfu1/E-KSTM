df507ab252cfad91ce8fbfaa40aa1907afa1c0a3
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-09-28 06:03:42 +0700

    Update index.php

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -72,8 +72,6 @@
         <!-- Masthead-->
         <header class="masthead bg-primary text-white text-center">
             <div class="container d-flex align-items-center flex-column">
-                <!-- Masthead Avatar Image-->
-                <img class="masthead-avatar mb-5" src="assets/img/avataaars.svg" alt="" />
                 <!-- Masthead Heading-->
                 <!-- <h1 class="masthead-heading text-uppercase mb-0">E-KSTM</h1> -->
                 <!-- Icon Divider-->
@@ -83,8 +81,7 @@
                 <!-- Masthead Subheading-->
                 <p class="masthead-subheading font-weight-light mb-0">
                   Platform Pengembangan dan Pengawasan <br>
-                  Program Kelompok Tani Santri Milenial (KSTM) <br>
-                  Sebagai Upaya Peningkatan Kualitas Program KSTM </p>
+                  Program Kelompok Tani Santri Milenial (KSTM)</p>
             </div>
         </header>
         <!-- Portfolio Section-->
