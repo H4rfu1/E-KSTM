ec8d860e6a5a1691d7407ed68a13bcb4d3147f76
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-09-27 15:59:10 +0700

    add lupa passwrod rooting

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -17,7 +17,7 @@ class Auth extends CI_Controller {
     $this->form_validation->set_rules('password', 'Password', 'trim|required', [
       'required' => "Password harus diisi"]);
     if($this->form_validation->run() == false){
-      $data['title'] = 'E-KSTM - User login';
+      $data['title'] = 'E-KSTM - Masuk';
       $this->load->view('templates/auth_header', $data);
       $this->load->view('auth/login');
       $this->load->view('templates/auth_footer');
@@ -88,9 +88,63 @@ class Auth extends CI_Controller {
     if($this->form_validation
     ->run() == false){
 
-      $data['title'] = 'E-KSTM - User Registration';
+      $data['title'] = 'E-KSTM - Daftar';
       $this->load->view('templates/auth_header', $data);
       $this->load->view('auth/registration');
+      $this->load->view('templates/auth_footer');
+    } else {
+      $email = $this->input->post('email', true);
+      $data = [
+        'name' => htmlspecialchars($this->input->post('fullname', true)),
+        'email' => htmlspecialchars($email),
+        'image' => 'default.jpg',
+        'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+        'role_id' => 2,
+        'is_active' => 0,
+        'date_create' => time()
+
+      ];
+      $pesan = '<div class="alert alert-success" role="alert"> Yey, Selamat '.$this->input->post('fullname').'!! akun kamu sudah terdaftar, email aktivasi sudah terkirim dan akan valid selama a24 jam. Coba cek pada bagian spam bila belum muncul. </div>';
+
+      //siapkan token
+      $token = base64_encode(random_bytes(32));
+      $user_token =[
+        'email' => $email,
+        'token' => $token,
+        'date_created' => time()
+      ];
+      $this->db->insert('user',$data);
+      $this->db->insert('user_token',$user_token);
+      $this->_sendEmail($token, 'verify');
+
+      $this->session-> set_flashdata('message', $pesan);
+      redirect('auth');
+    }
+  }
+
+  public function lupaPassword(){
+    if($this->session->userdata('email')){
+      redirect('user');
+    }
+    $this->form_validation->set_rules('fullname','Name', 'required|trim', [
+      'required' => "Nama harus diisi"]);
+    $this->form_validation->set_rules('email','Email', 'required|trim|valid_email|is_unique[user.email]',[
+      'required' => "Passsword lama harus diisi",
+      'required' => "Email harus diisi",
+      'is_unique' => 'Email sudah terdaftar'
+    ]);
+    $this->form_validation->set_rules('password1','Password','trim|min_length[3]|matches[password2]', [
+      'matches' => "Password tidak sama",
+      'min_length' => 'Password terlalu pendek'
+    ]);
+    $this->form_validation->set_rules('password2','Password2','trim|matches[password1]');
+
+
+    if($this->form_validation
+    ->run() == false){
+      $data['title'] = 'E-KSTM - Lupa Password';
+      $this->load->view('templates/auth_header', $data);
+      $this->load->view('auth/lupa_password');
       $this->load->view('templates/auth_footer');
     } else {
       $email = $this->input->post('email', true);
