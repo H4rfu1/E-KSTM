033b1114f2d51fac18c020721e01a0a4a8331bef
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-09-28 09:34:04 +0700

    Update index.php

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -2,8 +2,9 @@
 <html lang="en">
     <head>
         <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="Website “E-KSTM” ini akan sangat bermanfaat menjembatani pusat informasi antara KSTM dengan Dinas Ketahanan Pangan dan Peternakan Kabupaten Jember serta dapat mempermudah pengawasan (monitoring) berkala terhadap jalannya program KSTM." />
+        <meta name="description" content="" />
         <meta name="author" content="" />
         <title><?php echo $title; ?></title>
         <!-- Favicon-->
